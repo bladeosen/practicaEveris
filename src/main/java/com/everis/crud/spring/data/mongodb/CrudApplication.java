e6699ca154e7a48f993970a838b9eacbca7e8ff9
chris <chris@sinb.com>
2021-03-22 13:40:29 +0100

    Métodos implementados

--- src/main/java/com/everis/crud/spring/data/mongodb/CrudApplication.java
+++ src/main/java/com/everis/crud/spring/data/mongodb/CrudApplication.java
@@ -3,9 +3,6 @@ package com.everis.crud.spring.data.mongodb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 @SpringBootApplication
 public class CrudApplication {
 
